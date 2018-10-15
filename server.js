a400384a244b80d5f440fe3245804096270011ae
Sampsoni <sami.repo@live.com>
2018-10-15 17:29:15 +0300

    Record By ID

--- server.js
+++ server.js
@@ -8,42 +8,32 @@ var mongoose   = require('mongoose');
 var GameRecord     = require('./App/Models/game_record');
 mongoose.connect('mongodb://oh_web:Ketjukuul4@ds052978.mlab.com:52978/oh_record_service'); // connect to our database
 
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8080;
+
 
 // ROUTES FOR OUR API
 // =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
-// middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
     console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });   
 });
 
-// more routes for our API will happen here
-// on routes that end in /bears
-// ----------------------------------------------------
 router.route('/GameRecords')
 
-    // create a bear (accessed at POST http://localhost:8080/api/bears)
     .post(function(req, res) {
 
-        var record = new GameRecord();      // create a new instance of the Bear model
-        record.name = req.body.name;  // set the bears name (comes from the request)
+        var record = new GameRecord(); 
+        record.name = req.body.name; 
 
-        // save the bear and check for errors
         record.save(function(err) {
             if (err)
                 res.send(err);
@@ -53,7 +43,6 @@ router.route('/GameRecords')
 
     })
 
-    // get all the bears (accessed at GET http://localhost:8080/api/bears)
     .get(function(req, res) {
         GameRecord.find(function(err, bears) {
             if (err)
@@ -63,11 +52,17 @@ router.route('/GameRecords')
         });
     });
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+router.route('/GameRecords/:record_id')
+
+    .get(function(req, res) {
+        Bear.findById(req.params.record_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    });
+
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
