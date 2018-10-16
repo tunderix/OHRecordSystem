55d1974d3a22ccd5b0e344f229469f906fd7c666
Sampsoni <sami.repo@live.com>
2018-10-16 16:01:10 +0300

    Wait for stuff

--- App/Controllers/game_data.controller.js
+++ App/Controllers/game_data.controller.js
@@ -6,18 +6,16 @@ exports.defaultGameData = (req, res) => {
     return res.send(gData);
 };
 
-
-
-
   
 exports.collectedGameData = (req, res) => {
     var allData = [];
-    var data = [EncounterController.findAll, encounterChoices];
+    var data = [EncounterController.findAll];
 
     Promise.all(data.map(partialData => {
         allData.push(partialData);
     }))
     .then(result => {
+        allData.push(encounterChoices);
         res.send(allData);
     })
     .catch(console.error);
