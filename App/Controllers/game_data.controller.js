7381e1ba1989215127ab53360284245f7c1e8d28
Sampsoni <sami.repo@live.com>
2018-10-16 15:29:24 +0300

    s

--- App/Controllers/game_data.controller.js
+++ App/Controllers/game_data.controller.js
@@ -2,16 +2,32 @@ const gData = require('../GameData/GameData.json');
 const encounterChoices = require('../GameData/EncounterChoices.json');
 const EncounterController = require('./gd_encounter.controller.js');
 
-
 exports.defaultGameData = (req, res) => {
     return res.send(gData);
 };
 
+
+
+
+  
 exports.collectedGameData = (req, res) => {
     var allData = [];
-    allData.push(encounterChoices[0]);
-    allData.push(EncounterController.findAll);
-    return res.send(allData);
+
+    var promises = [EncounterController.findAll, encounterChoices].map(function(data) {
+        return new Promise(function(resolve, reject) {
+          var collection = conn.collection(data);
+          collection.drop(function(err) {
+            if (err) { return reject(err); }
+            allData.push(data);
+            console.log('dropped ' + data);
+            resolve();
+          });
+        });
+      });
+
+    Promise.all(promises)
+    .then(function() { return res.send(allData); })
+    .catch(console.error);
 };
 
 
