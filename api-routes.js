0e799affdd1247fb50597858bc5e564e2050e493
Sampsoni <sami.repo@live.com>
2018-10-16 13:52:48 +0300

    Waau

--- api-routes.js
+++ api-routes.js
@@ -10,29 +10,29 @@ router.get('/', function (req, res) {
     });
 });
 
-module.exports = (app) => {
-    const GameRecordController = require('./App/Controllers/game_record.controller.js');
-    const APIController = require('./App/Controllers/api.controller.js');
-    const GameDataController = require('./App/Controllers/game_data.controller.js');
-    const EncounterController = require('./App/Controllers/gd_encounter.controller.js');
+module.exports = (App) => {
+    const GameRecordController = require('./Controllers/game_record.controller.js');
+    const APIController = require('./Controllers/api.controller.js');
+    const GameDataController = require('./Controllers/game_data.controller.js');
+    const EncounterController = require('./Controllers/gd_encounter.controller.js');
 
-    app.get('/', APIController.apiDescription)
+    App.get('/', APIController.apiDescription)
     
     // Game Records
-    app.post('/records', GameRecordController.create);
-    app.get('/records', GameRecordController.findAll);
-    app.get('/records/:record_id', GameRecordController.findOne);
-    app.put('/records/:record_id', GameRecordController.update);
-    app.delete('/records/:record_id', GameRecordController.delete);
+    App.post('/records', GameRecordController.create);
+    App.get('/records', GameRecordController.findAll);
+    App.get('/records/:record_id', GameRecordController.findOne);
+    App.put('/records/:record_id', GameRecordController.update);
+    App.delete('/records/:record_id', GameRecordController.delete);
 
     // Game Data
-    app.get('/gamedata', GameDataController.defaultGameData);
-    app.get('/gamedata/collected', GameDataController.collectedGameData);
+    App.get('/gamedata', GameDataController.defaultGameData);
+    App.get('/gamedata/collected', GameDataController.collectedGameData);
     
     // Encounters
-    app.post('/gamedata/encounters', EncounterController.create);
-    app.get('/gamedata/encounters', EncounterController.findAll);
-    app.get('/gamedata/encounters/:encounter_id', EncounterController.findOne);
-    app.put('/gamedata/encounters/:encounter_id', EncounterController.update);
-    app.delete('/gamedata/encounters/:encounter_id', EncounterController.delete);
+    App.post('/gamedata/encounters', EncounterController.create);
+    App.get('/gamedata/encounters', EncounterController.findAll);
+    App.get('/gamedata/encounters/:encounter_id', EncounterController.findOne);
+    App.put('/gamedata/encounters/:encounter_id', EncounterController.update);
+    App.delete('/gamedata/encounters/:encounter_id', EncounterController.delete);
 };
